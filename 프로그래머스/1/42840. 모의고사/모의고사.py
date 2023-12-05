160bdda6743f96df2a50a2b80e949030d9ac0e51
이한석 / Hanseok Lee <onestone0311@gmail.com>
2023-12-05 13:17:39 +0900

    [level 1] Title: 모의고사, Time: 3.91 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,3 @@
 def solution(answers):
-    answers = answers
-    no_math_1 = [1,2,3,4,5] * 2000
-    no_math_2 = [2,1,2,3,2,4,2,5] * 1250 
-    no_math_3 = [3,3,1,1,2,2,4,4,5,5] * 1000
-    
-    math_1, math_2, math_3 = 0, 0, 0
-
-    for i in range(len(answers)) :
-        if no_math_1[i] == answers[i] : math_1 += 1
-        if no_math_2[i] == answers[i] : math_2 += 1
-        if no_math_3[i] == answers[i] : math_3 += 1
-    
-    tmp = [math_1, math_2, math_3]
-    
     answer = []
-    
-    for i in range(len(tmp)) :
-        if tmp[i] == max(tmp) :
-            answer.append( i+1 )
-
     return answer
