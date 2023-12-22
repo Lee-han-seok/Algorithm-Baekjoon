1c35b9b778d60b48e466c05ea33ebc39724c64ac
이한석 / Hanseok Lee <onestone0311@gmail.com>
2023-12-22 09:05:22 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,7 +1,9 @@
 num = int(input())
 
+# 점화식 : a(n) = a(n-1) + a(n-2)
+
 result_list = [1, 2]
-for i in range(2, 1000) :
+for i in range(2, num + 1) :
     result_list.append((result_list[i - 2] + result_list[i-1]) % 10007)
 
 print(result_list[num-1])
