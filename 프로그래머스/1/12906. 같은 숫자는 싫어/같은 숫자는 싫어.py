2b072a484171aa03bfb46699f9a1ff4538a321a1
이한석 / Hanseok Lee <onestone0311@gmail.com>
2024-06-16 21:36:24 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 102.21 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,4 +1,3 @@
-#arr = [1, 1, 3, 3, 0, 1, 1]
 def solution(arr):
     answer = []
     answer.append(arr[0])
@@ -7,22 +6,3 @@ def solution(arr):
             answer.append(arr[i])
         else : pass
     return answer
-
-
-
-
-
-
-
-
-
-
-# def solution(arr):
-#     answer = []
-#     answer.append(arr[0])
-#     for i in range(len(arr)-1) :
-#         if arr[i] == arr[i+1] :
-#             pass
-#         if arr[i] != arr[i+1] :
-#             answer.append(arr[i+1])
-#     return answer
